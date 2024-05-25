3f8f2fc8b0bf5a704b96ec30fa7893b8b6b0bc73
Микола Столярчук <koliastoliarchuk@gmail.com>
2024-05-25 12:02:04 +0300

    Create Models

---
+++ Models/CryptoDetailModel.cs
@@ -0,0 +1,32 @@
+﻿using coinA.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace coinA.Models
+{
+    public class CryptoDetailModel : ViewModelBase
+    {
+        public string Name { get; set; }
+        public string Symbol { get; set; }
+        public string Image { get; set; }
+        public decimal CurrentPrice { get; set; }
+        public decimal Volume { get; set; }
+        public List<Market> Markets { get; set; }
+        public List<PriceChange> PriceChanges { get; set; }
+    }
+
+    public class Market
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class PriceChange
+    {
+        public DateTime Timestamp { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ Models/TopCryptoModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace coinA.Models
+{
+    public class TopCryptoModel
+    {
+        public string Id { get; set; }
+        public string Symbol { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public decimal CurrentPrice { get; set; }
+        public long MarketCap { get; set; }
+        public long TotalVolume { get; set; }
+
+    }
+}
