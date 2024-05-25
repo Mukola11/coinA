4eaee7ce30a76760c8f7f9c01dda5d2109e37706
Микола Столярчук <koliastoliarchuk@gmail.com>
2024-05-25 12:17:50 +0300

    Add ViewModelBase with INotifyPropertyChanged implementation

---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,22 @@
+﻿using coinA.Commands;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace coinA.ViewModels
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChange(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
