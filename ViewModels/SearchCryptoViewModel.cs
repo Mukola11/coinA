76ecfccffc2337859c2cfe639e88ae0a8fa3d050
Микола Столярчук <koliastoliarchuk@gmail.com>
2024-05-25 12:19:08 +0300

    Add VewModels

---
+++ ViewModels/SearchCryptoViewModel.cs
@@ -0,0 +1,68 @@
+﻿using coinA.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace coinA.ViewModels
+{
+    public class SearchCryptoViewModel : ViewModelBase
+    {
+        private string _searchText;
+        private ObservableCollection<SearchCryptoModel> _searchResults;
+        private ObservableCollection<SearchCryptoModel> _allCryptos;
+
+        public string SearchText
+        {
+            get { return _searchText; }
+            set
+            {
+                _searchText = value;
+                OnPropertyChange(nameof(SearchText));
+                FilterSearchResults();
+            }
+        }
+
+        public ObservableCollection<SearchCryptoModel> SearchResults
+        {
+            get { return _searchResults; }
+            set
+            {
+                _searchResults = value;
+                OnPropertyChange(nameof(SearchResults));
+            }
+        }
+
+        public SearchCryptoViewModel()
+        {
+            // Ініціалізація колекції всіх криптовалют
+            _allCryptos = new ObservableCollection<SearchCryptoModel>
+        {
+            new SearchCryptoModel { Symbol = "btc", Name = "Bitcoin" , Image = "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1696501400"},
+            new SearchCryptoModel { Symbol = "eth", Name = "Ethereum", Image = "https://assets.coingecko.com/coins/images/279/large/ethereum.png?1696501628" },
+            new SearchCryptoModel { Symbol = "xrp", Name = "Ripple", Image = "https://assets.coingecko.com/coins/images/44/large/xrp-symbol-white-128.png"},
+        };
+
+            // Ініціалізація результатів пошуку пустою колекцією
+            SearchResults = new ObservableCollection<SearchCryptoModel>();
+        }
+
+        private void FilterSearchResults()
+        {
+            if (string.IsNullOrEmpty(SearchText))
+            {
+                SearchResults = new ObservableCollection<SearchCryptoModel>(_allCryptos);
+            }
+            else
+            {
+                var filteredResults = _allCryptos
+                    .Where(c => c.Symbol.StartsWith(SearchText, StringComparison.OrdinalIgnoreCase) ||
+                                c.Name.StartsWith(SearchText, StringComparison.OrdinalIgnoreCase))
+                    .ToList();
+                SearchResults = new ObservableCollection<SearchCryptoModel>(filteredResults);
+            }
+        }
+    }
+}
---
+++ ViewModels/TopCryptoViewModel.cs
@@ -0,0 +1,87 @@
+﻿using coinA.Commands;
+using coinA.Models;
+using coinA.Stores;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace coinA.ViewModels
+{
+
+
+    public class TopCryptoViewModel : ViewModelBase
+    {
+        private string _searchText;
+
+        private ObservableCollection<TopCryptoModel> _topCryptoModel;
+
+        public ObservableCollection<TopCryptoModel> TopCryptoModel
+        {
+            get
+            {
+                return _topCryptoModel;
+            }
+            set
+            {
+                _topCryptoModel = value;
+                OnPropertyChange(nameof(TopCryptoModel));
+            }
+        }
+
+        public string SearchText
+        {
+            get => _searchText;
+            set
+            {
+                _searchText = value;
+                OnPropertyChange(nameof(SearchText));
+                if (!string.IsNullOrEmpty(_searchText))
+                {
+                    OnSearchCommand.Execute(null);
+                }
+            }
+        }
+
+        public ICommand OnSearchCommand { get; }
+
+        public ICommand OnDetailCommand { get; }
+
+        public TopCryptoViewModel(NavigationStore navigationStore)
+        {
+            OnSearchCommand = new OnSearchCommand(navigationStore, this);
+
+            OnDetailCommand = new OnDetailCommand(navigationStore);
+
+            TopCryptoModel = new ObservableCollection<TopCryptoModel>
+            {
+                new TopCryptoModel
+                {
+                    Id = "bitcoin",
+                    Symbol = "btc",
+                    Name = "Bitcoin",
+                    Image = "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1696501400",
+                    CurrentPrice = 70005,
+                    MarketCap = 1379506765043,
+                    TotalVolume = 33785648257
+                },
+                new TopCryptoModel
+                {
+                    Id = "ethereum",
+                    Symbol = "eth",
+                    Name = "Ethereum",
+                    Image = "https://assets.coingecko.com/coins/images/279/large/ethereum.png?1696501628",
+                    CurrentPrice = 3742.31M,
+                    MarketCap = 450267431131,
+                    TotalVolume = 32803629327
+                }
+
+            };
+        }
+
+
+    }
+}
