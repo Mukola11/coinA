fd2f4a37b9696c834889e4f78e2e96254152f980
Микола Столярчук <koliastoliarchuk@gmail.com>
2024-05-27 02:50:55 +0300

    Add OpenUrlCommand to handle opening URLs in browser

---
+++ Commands/OpenUrlCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace coinA.Commands
+{
+    public class OpenUrlCommand : CommandBase
+    {
+        public override void Execute(object parameter)
+        {
+            if (parameter is string url)
+            {
+                Process.Start(url);
+            }
+        }
+    }
+}
