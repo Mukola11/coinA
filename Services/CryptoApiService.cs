fa663a938ae19af5f20ca2f80f3fbe52948e3e55
Микола Столярчук <koliastoliarchuk@gmail.com>
2024-05-26 19:42:54 +0300

    Add HTTP request logic to CryptoApiService:

---
+++ Services/CryptoApiService.cs
@@ -0,0 +1,161 @@
+﻿using coinA.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace coinA.Services
+{
+    public class CryptoApiService
+    {
+        private readonly HttpClient _httpClient;
+
+        public CryptoApiService()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<List<TopCryptoModel>> GetTopCryptosAsync()
+        {
+            try
+            {
+                var request = new HttpRequestMessage(HttpMethod.Get, CryptoApiUrls.GetTopCryptosUrl());
+                request.Headers.Add("User-Agent", "coinA");
+
+                var response = await _httpClient.SendAsync(request);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseBody = await response.Content.ReadAsStringAsync();
+                    return JsonConvert.DeserializeObject<List<TopCryptoModel>>(responseBody);
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+
+        public async Task<CryptoDetailModel> GetCryptoDetailAsync(string id)
+        {
+            try
+            {
+                var cryptoDetail = await FetchCryptoDetailAsync(id);
+                if (cryptoDetail != null)
+                {
+                    var priceChanges = await FetchPriceChangesAsync(id);
+                    if (priceChanges != null)
+                    {
+                        cryptoDetail.PriceChanges = priceChanges;
+                    }
+                }
+
+                return cryptoDetail;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+
+        private async Task<CryptoDetailModel> FetchCryptoDetailAsync(string id)
+        {
+            try
+            {
+                string url = CryptoApiUrls.GetCryptoDetailUrl(id);
+                var request = new HttpRequestMessage(HttpMethod.Get, url);
+                request.Headers.Add("User-Agent", "coinA");
+
+                var response = await _httpClient.SendAsync(request);
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseBody = await response.Content.ReadAsStringAsync();
+                    return JsonConvert.DeserializeObject<CryptoDetailModel>(responseBody);
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+
+        private async Task<List<PriceChange>> FetchPriceChangesAsync(string id)
+        {
+            try
+            {
+                string url = CryptoApiUrls.GetCryptoPriceChangesUrl(id);
+                var request = new HttpRequestMessage(HttpMethod.Get, url);
+                request.Headers.Add("User-Agent", "coinA");
+
+                var response = await _httpClient.SendAsync(request);
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseBody = await response.Content.ReadAsStringAsync();
+                    var priceChangeData = JsonConvert.DeserializeObject<JObject>(responseBody);
+
+                    var priceChangeArray = priceChangeData["prices"] as JArray;
+                    var priceChanges = new List<PriceChange>();
+                    foreach (var item in priceChangeArray)
+                    {
+                        var timestamp = item[0].ToObject<long>();
+                        var price = item[1].ToObject<decimal>();
+                        priceChanges.Add(new PriceChange { Timestamp = timestamp, Price = price });
+                    }
+
+                    return priceChanges;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+
+        public async Task<List<SearchCryptoModel>> SearchCryptosAsync(string query)
+        {
+            try
+            {
+                string url = CryptoApiUrls.GetSearchCryptoUrl(query);
+                var request = new HttpRequestMessage(HttpMethod.Get, url);
+                request.Headers.Add("User-Agent", "coinA");
+
+                var response = await _httpClient.SendAsync(request);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseBody = await response.Content.ReadAsStringAsync();
+                    var searchResponse = JsonConvert.DeserializeObject<SearchResponse>(responseBody);
+                    return searchResponse.Coins;
+                }
+                else
+                {
+                    return new List<SearchCryptoModel>();
+                }
+            }
+            catch
+            {
+                return new List<SearchCryptoModel>();
+            }
+        }
+    }
+}
---
+++ Services/CryptoApiUrls.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace coinA.Services
+{
+    public static class CryptoApiUrls
+    {
+        public const string BaseUrl = "https://api.coingecko.com/api/v3/";
+        public static string GetTopCryptosUrl() => $"{BaseUrl}coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false";
+        public static string GetCryptoDetailUrl(string id) => $"{BaseUrl}coins/{id}";
+        public static string GetCryptoPriceChangesUrl(string id) => $"{BaseUrl}coins/{id}/market_chart?vs_currency=usd&days=1";
+        public static string GetSearchCryptoUrl(string query) => $"{BaseUrl}search?query={query}";
+
+    }
+}
